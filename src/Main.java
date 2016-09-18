d928abb39dbcb10df3eeada0806bdd18564391da
Vincent <artemshylov93@gmail.com>
2016-09-18 12:59:25 +0300

    I'm done with it

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,5 @@ public class Main {
     public static void main(String[] args) {
         Crew newCrew = new Crew(new Human(24,"Vincent Vega"), new Human(25,"Elise"), "Rainbow6");
         System.out.println(newCrew.toString());
-        System.out.println("asdasdasdd");
     }
 }
