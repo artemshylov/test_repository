1e4258969c5dcddf3797ae9e882f38654a9ef48e
Vincent <artemshylov93@gmail.com>
2016-09-18 14:42:12 +0300

    asd

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * @author V.Vega
  * @author www.artemshylov.net
  */
 public class Main{
-
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Hi");
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300,300);
+        frame.setBackground(Color.GREEN);
+        frame.setLocation(400,400);
+    }
 }
